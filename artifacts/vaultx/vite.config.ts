9bc4e6fc25f1c43743932c04a7ec3577efd3f850
wggg01-dev <wggg01@gmail.com>
2026-04-29 19:29:53 +0000

    env change

--- artifacts/vaultx/vite.config.ts
+++ artifacts/vaultx/vite.config.ts
@@ -8,8 +8,8 @@ const rawPort = process.env.PORT;
 
 if (!rawPort) {
   throw new Error(
-    "PORT environment variable is required but was not provided.",
-  );
+    "PORT environment variable is required but was not provided."),
+    };
 }
 
 const port = Number(rawPort);
